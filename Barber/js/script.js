9f8d97a84b8e50618fb474ca0d1b83ad0e97252c
Yaroslav Ivanov <me@yaroslav-i.ru>
2018-05-28 00:50:30 +0300

    add changes to barber

--- Barber/js/script.js
+++ Barber/js/script.js
@@ -15,4 +15,24 @@ $(document).ready(function(){
   	margin: 60
   });
 
+
+
+  $(".js-pricing-block").each(function() {
+    var tab = $(this).find(".tabs__item");
+    var content = $(this).find(".tabs-content");
+
+    tab.on("click", function() {
+      var index = $(this).index();
+
+      tab.removeClass("tabs__item--active");
+      $(this).addClass("tabs__item--active");
+
+      content.hide();
+      content.eq(index).css({"display":"flex"});
+    });
+
+    // Эмуляция клика на первый элементы
+    tab.first().click();
+  })
+
 });
