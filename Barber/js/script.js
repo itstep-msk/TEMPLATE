cbe193490b49930fca4d630d9bbdeccba059b7a2
Yaroslav Ivanov <me@yaroslav-i.ru>
2018-05-27 19:35:38 +0300

    add changes to barber

--- Barber/js/script.js
+++ Barber/js/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+
   $(".js-specialists-block").owlCarousel({
   	loop: true,
   	margin: 60
   });
+
+  $(".js-reviews-block").owlCarousel({
+  	loop: true,
+  	items: 1
+  });
+
+  $(".js-team-block").owlCarousel({
+  	loop: true,
+  	margin: 60
+  });
+
 });
